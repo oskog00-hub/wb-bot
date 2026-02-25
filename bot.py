e8ed55c1a001ce96a2aa01a20e76fb08e10f3199
oskog00-hub <o.skog00@gmail.com>
2026-02-25 12:53:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,42 @@
 import asyncio
 import os
-import aiosqlite
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+import aiosqlite
 
-TOKEN = os.environ.get("TOKEN")
+TOKEN = os.getenv("TOKEN")
 
-if TOKEN is None:
+if not TOKEN:
     print("TOKEN NOT FOUND")
-    raise SystemExit(1)
+    exit()
 
 print("TOKEN OK")
+print("BOT STARTING...")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 DB = "users.db"
 
-
+# создаём базу
 async def init_db():
     async with aiosqlite.connect(DB) as db:
-        await db.execute(
-            "CREATE TABLE IF NOT EXISTS users (user_id INTEGER)"
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER
         )
+        """)
         await db.commit()
 
-
+# ответ только на /start
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer("WB бот работает 24/7 🚀")
 
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer("Работаю.")
-
-
 async def main():
-    print("BOT STARTING...")
     await init_db()
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
